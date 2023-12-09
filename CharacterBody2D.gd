7210ca15c52dc38f80c5076a83c244f4c07f71b9
mpallansch <matthew.pallansch@gmail.com>
2023-12-09 15:19:53 -0500

    Begins Process of Randomizing Levels

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -800.0
+const SPEED = 650.0
+const JUMP_VELOCITY = -1600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 3
 
 
 func _physics_process(delta):
