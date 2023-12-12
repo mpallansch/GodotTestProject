90ca3f906100a197fee9562d8af444e37fb7153e
mpallansch <matthew.pallansch@gmail.com>
2023-12-11 23:33:40 -0500

    Adds Animations

--- Scenes/Entities/Player/CharacterBody2D.gd
+++ Scenes/Entities/Player/CharacterBody2D.gd
@@ -3,9 +3,15 @@ extends CharacterBody2D
 const NAME = "Player"
 const JUMP_VELOCITY = -1600.0
 
+@onready var animated_sprite = %AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 3
 
+func _ready():
+	$AnimationPlayer.play("idle")
+	pass # Replace with function body.
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -13,7 +19,9 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$AnimationPlayer.play("jump")
 		velocity.y = JUMP_VELOCITY
+			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,5 +30,17 @@ func _physics_process(delta):
 		velocity.x = direction * PlayerVariables.current_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, PlayerVariables.current_speed)
-
+		
+	if !Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			if direction != 0:
+				if $AnimationPlayer.current_animation != "run":
+					$AnimationPlayer.play("run")
+			else:
+				if $AnimationPlayer.current_animation != "idle":
+					$AnimationPlayer.play("idle")
+					
+	#if direction < 0:
+		#if
+					
 	move_and_slide()
