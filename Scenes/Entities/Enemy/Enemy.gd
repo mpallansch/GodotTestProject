ede643a24349c7848b6dd988133f28a3f31269bd
mpallansch <matthew.pallansch@gmail.com>
2023-12-13 14:46:08 -0500

    Implements Enemy Health Bar

--- Scenes/Entities/Enemy/Enemy.gd
+++ Scenes/Entities/Enemy/Enemy.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 const experience = 5
 const projectile_interval = 3
-const max_health = 10
+const max_health = 15.0
 
 signal enemy_apply_damage(damage)
 
 @onready var projectile_scene = preload("res://Scenes/Entities/Enemy/Projectile.tscn")
+@onready var health_bar = %HealthBar
+@onready var health_bar_container = %HealthBarContainer
 
 var health = max_health
 var delay = 0;
@@ -14,6 +16,7 @@ var delay = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	enemy_apply_damage.connect(on_damage)
+	update_heatlh_bar()
 	pass # Replace with function body.
 
 
@@ -37,6 +40,10 @@ func _on_area_2d_body_entered(body):
 
 func on_damage(damage):
 	health -= damage
+	update_heatlh_bar()
 	if health <= 0:
 		PlayerVariables.increase_experience(experience)
 		queue_free()
+		
+func update_heatlh_bar():
+	health_bar.size.x = health_bar_container.size.x * (health / max_health)
