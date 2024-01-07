13ce0c7f0962001fdf5927d21e246b3d6454aa86
mpallansch <matthew.pallansch@gmail.com>
2024-01-07 12:06:21 -0500

    Fix for Background Music

--- Scenes/Room.gd
+++ Scenes/Room.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 signal initialize(from_exit, persistent_data, seed)
 
+var is_enemy_present
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("initialize", on_initialize)
@@ -36,11 +38,15 @@ func process_nodes(node, seed):
 		if child_node.has_meta("spawn_chance"):
 			if rng.randf_range(0, 1) > child_node.get_meta("spawn_chance"):
 				child_node.queue_free()
+				return
 				
 		if child_node.has_meta("min_floor"):
 			if PlayerVariables.floor < child_node.get_meta("min_floor"):
 				child_node.queue_free()
+				return
 				
+		if child_node.name.ends_with("Enemy"):
+			is_enemy_present = true
 			
 		if child_node.get_child_count() > 0:
 			process_nodes(child_node, seed)
@@ -58,5 +64,8 @@ func on_initialize(from_exit, persistent_state, seed):
 			var node_receiving_state = get_node(path)
 			if node_receiving_state:
 				node_receiving_state.emit_signal("apply_persistent_state", persistent_state[path])
-		
+	
+	is_enemy_present = false
 	process_nodes(self, seed)
+	AudioManager.adjust_enemy_music(is_enemy_present)
+	
